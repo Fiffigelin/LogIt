cedcc6b608e0411ffaca4a5ba7872d1cc3253ff3
Elin Nyman <elin.nyman87@gmail.com>
2026-01-09 09:59:07 +0100

    refactor(home): changed from swedish to english

--- Frontend/src/pages/private/home.tsx
+++ Frontend/src/pages/private/home.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="flex justify-center items-center h-full">
-      <h1 className="text-3xl font-stretch-90% text-gray-800">INLOGGAD</h1>
+      <h1 className="text-3xl font-stretch-90% text-gray-800">LOGGED IN</h1>
     </div>
   );
 }
